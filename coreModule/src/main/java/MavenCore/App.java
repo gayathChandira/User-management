45f3d7054a517f512591f0b807cca14600542e45
Gayath Chandira <gayath@gmail.com>
2019-08-19 13:01:55 +0530

    first commit

---
+++ coreModule/src/main/java/MavenCore/App.java
@@ -0,0 +1,13 @@
+package MavenCore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+##User-Management Project
+
+This project contains 
+* Core module
+    + to run, java -jar coreModule.jar
+* Web module
+    + to run, mvn jetty:run 
+
